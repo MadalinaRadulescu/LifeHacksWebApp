9f2a17f5236e437d796ba9ec77038a49c407c31c
MadalinaRadulescu <radulescumadalina90@yahoo.com>
2023-02-27 14:59:46 +0200

    cookies

---
+++ frontend/src/Pages/Logout/Logout.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Logout = () => {
+  let navigate = useNavigate();
+  useEffect(() => {
+    fetch("http://localhost:5260/api/Auth/Logout", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      mode: "cors",
+    })
+      .then((response) => response.json())
+      .then((r) => {
+        console.log(r)
+        let path = `/`;
+        document.cookie =
+          "jwt= ; Path=/; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+        navigate(path);
+      });
+  }, [navigate]);
+
+  return <></>;
+};
+
+export default Logout;
